f5717b37a1acb7f1a7d9f14b7fff19402fb6f300
sharmiladevarajan <devarajsharmila36@gmail.com>
2026-01-03 18:16:51 +0530

    removed the db query and data

--- add_image_url_column.sql
+++
@@ -1,5 +0,0 @@
--- Add image_url column to movies table
-ALTER TABLE movies ADD COLUMN IF NOT EXISTS image_url VARCHAR(500);
-
--- Add comment to document the column
-COMMENT ON COLUMN movies.image_url IS 'URL to the movie poster/cover image';
